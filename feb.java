6894cce71cc7a57167e3beb645bdd641898ec029
@SethSandesh <sandeshseth6@gmail.com>
2024-02-22 17:02:41 +0530

    change

--- feb.java
+++ feb.java
@@ -1,29 +1,15 @@
-// Fibonacci series program in java
+public class AddTwoNumbers {
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 7;
 
-class GFG {
-	// Function to print N Fibonacci Number
-	static void Fibonacci(int N)
-	{
-		int num1 = 0, num2 = 1;
-	
-		for (int i = 0; i < N; i++) {
-			// Print the number
-			System.out.print(num1 + " ");
+        int sum = add(num1, num2);
 
-			// Swap
-			int num3 = num2 + num1;
-			num1 = num2;
-			num2 = num3;
-		}
-	}
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
 
-	// Driver Code
-	public static void main(String args[])
-	{
-		// Given Number N
-		int N = 10;
-
-		// Function Call
-		Fibonacci(N);
-	}
+    public static int add(int a, int b) {
+        return a + b;
+    }
 }
+
